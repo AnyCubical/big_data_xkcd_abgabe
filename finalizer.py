16a6aca25d58d9ee24c310663c4d67895de74a68
AnyCubical <56519373+AnyCubical@users.noreply.github.com>
2022-12-02 00:02:29 +0100

    Create finalizer.py
    Reduzieren und Optimieren der Rohdateien

---
+++ finalizer.py
@@ -0,0 +1,29 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+import argparse
+from pyspark.sql.functions import desc
+import pandas
+import sqlite3
+
+if __name__ == '__main__':
+    con = sqlite3.connect("/home/airflow/mnt/xkcd.db")
+
+    sc = pyspark.SparkContext()
+    spark = SparkSession(sc)
+    test = spark.read.format('json')\
+        .option('recursiveFileLookup',True)\
+        .load('/user/hadoop/xkcd/raw/*')
+
+    xkcd = test.drop("alt").drop("day").drop("link").drop("month").drop("news").drop("transcript").drop("extra_parts").drop("title")
+
+    xkcd.printSchema()
+    xkcd.write.format('csv')\
+        .mode('overwrite')\
+        .option('path', '/user/hadoop/xkcd/final')\
+        .saveAsTable('default.xkcd_partitioned')\
+
+    df = xkcd.toPandas()
+    
+    df.to_sql("xkcd", con, if_exists="replace")
+    con.close()
